88af18d262991647496835523a68f3cb0cdfc89b
grtvd <grtvd@users.noreply.github.com>
2005-12-16 17:20:52 -0500

    webapi: Added logic to load cache of DatabaseApdators.

--- src/com/inetvod/playerapi/PlayerXmlServlet.java
+++ src/com/inetvod/playerapi/PlayerXmlServlet.java
@@ -4,8 +4,8 @@
  */
 package com.inetvod.playerapi;
 
-import java.io.IOException;
 import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +13,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.inetvod.common.core.ServletFulfiller;
+import com.inetvod.common.dbdata.Category;
+import com.inetvod.common.dbdata.Member;
+import com.inetvod.common.dbdata.MemberPrefs;
+import com.inetvod.common.dbdata.MemberProvider;
+import com.inetvod.common.dbdata.MemberSession;
+import com.inetvod.common.dbdata.Provider;
+import com.inetvod.common.dbdata.Rating;
+import com.inetvod.common.dbdata.RentedShow;
+import com.inetvod.common.dbdata.SerialNumber;
+import com.inetvod.common.dbdata.Show;
+import com.inetvod.common.dbdata.ShowCategory;
+import com.inetvod.common.dbdata.ShowProvider;
 import com.inetvod.player.request.PlayerServletFulfiller;
 import org.apache.log4j.xml.DOMConfigurator;
 
@@ -29,6 +41,20 @@ public class PlayerXmlServlet extends HttpServlet
 		catch(MalformedURLException e)
 		{
 		}
+
+		// Preload DatabaseAdaptors
+		Provider.getDatabaseAdaptor();
+		Category.getDatabaseAdaptor();
+		Rating.getDatabaseAdaptor();
+		Member.getDatabaseAdaptor();
+		MemberPrefs.getDatabaseAdaptor();
+		SerialNumber.getDatabaseAdaptor();
+		MemberSession.getDatabaseAdaptor();
+		MemberProvider.getDatabaseAdaptor();
+		Show.getDatabaseAdaptor();
+		ShowProvider.getDatabaseAdaptor();
+		ShowCategory.getDatabaseAdaptor();
+		RentedShow.getDatabaseAdaptor();
 	}
 
 	protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
