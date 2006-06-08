b02b848990e8e74980ac2baebce4e4096e26493d
grtvd <grtvd@users.noreply.github.com>
2006-06-07 20:45:02 -0400

    (Part 2) Moved com/inetvod/common (core, data, dbdata) to shared/java/com/inetvod/common. Created shared/java/com/inetvod/common/web, move ServletFulfiller.java into. Updated project files.

--- src/com/inetvod/common/core/DataID.java
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright © 2004-2006 iNetVOD, Inc. All Rights Reserved.
- * iNetVOD Confidential and Proprietary.  See LEGAL.txt.
- */
-package com.inetvod.common.core;
-
-public abstract class DataID
-{
-}
